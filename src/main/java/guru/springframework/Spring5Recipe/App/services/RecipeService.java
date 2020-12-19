4b910204ffc502b522b2e1f63fc2ddfb7c5468a6
Jan <jan.witek88@gmail.com>
2020-12-19 14:42:30 +0100

    index doesnt work

--- src/main/java/guru/springframework/Spring5Recipe/App/services/RecipeService.java
+++ src/main/java/guru/springframework/Spring5Recipe/App/services/RecipeService.java
@@ -2,10 +2,13 @@ package guru.springframework.Spring5Recipe.App.services;
 
 import guru.springframework.Spring5Recipe.App.domain.Recipe;
 
+import java.util.Optional;
 import java.util.Set;
 
 
 public interface RecipeService {
 
     Set<Recipe> getRecipe();
+
+    Recipe findById(Long l);
 }
--- src/test/java/guru/springframework/Spring5Recipe/App/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/Spring5Recipe/App/services/RecipeServiceImplTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,5 +39,22 @@ class RecipeServiceImplTest {
         Set<Recipe> recipes = recipeService.getRecipe();
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+    }
+
+
+    @Test
+    void findById() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull(recipeReturned, "Null Recipe Returned");
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
     }
 }
