a5a22a85a1e4ca8c0709b4564a467182cd9f0115
Jan <jan.witek88@gmail.com>
2020-12-26 14:46:04 +0100

    Upload Images done.

---
+++ src/main/java/guru/springframework/Spring5Recipe/App/services/ImageService.java
@@ -0,0 +1,8 @@
+package guru.springframework.Spring5Recipe.App.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
