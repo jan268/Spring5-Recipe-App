7347568560e796be292be815041d71b483bb4731
Jan <jan.witek88@gmail.com>
2020-12-27 10:41:42 +0100

    Upload Image to DB.

--- src/main/java/guru/springframework/Spring5Recipe/App/services/ImageService.java
+++ src/main/java/guru/springframework/Spring5Recipe/App/services/ImageService.java
@@ -1,8 +1,10 @@
 package guru.springframework.Spring5Recipe.App.services;
 
+import guru.springframework.Spring5Recipe.App.commands.RecipeCommand;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
 
     void saveImageFile(Long recipeId, MultipartFile file);
+
 }
