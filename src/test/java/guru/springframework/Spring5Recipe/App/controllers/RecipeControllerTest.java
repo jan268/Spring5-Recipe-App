000be5afb6f07539aec169f7f8010f27f23b1dca
Jan <jan.witek88@gmail.com>
2021-01-01 16:33:55 +0100

    Error Messages and get to know I18N

--- src/test/java/guru/springframework/Spring5Recipe/App/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/Spring5Recipe/App/controllers/RecipeControllerTest.java
@@ -90,6 +90,7 @@ class RecipeControllerTest {
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
+                .param("cookTime", "3000")
         )
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("recipe"))
